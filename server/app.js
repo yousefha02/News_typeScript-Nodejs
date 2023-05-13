e11a446173a121d7503e20690135004b07d5a91e
yousefha02 <yousefha2029@gmail.com>
2023-05-13 14:59:57 +0300

    opinion && author

--- server/app.js
+++ server/app.js
@@ -53,7 +53,7 @@ app.use((error,req,res,next)=>
 {
     console.log(error);
     const status = error.statusCode
-    const message = error.message
+    const message = error.message 
     res.status(status).json({message:message})
 })
 
