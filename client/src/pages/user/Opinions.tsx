07873d565c2a4ae2f3c9850254f18ccb54963c88
moh7amed2002 <moha7medheshamabohani@gmail.com>
2023-05-22 17:54:06 +0300

    opinions page

--- client/src/pages/user/Opinions.tsx
+++ client/src/pages/user/Opinions.tsx
@@ -4,37 +4,34 @@ import HeaderTitle from '../../components/reusableUi/HeaderTitle'
 import { Box, Container, Grid } from '@mui/material'
 import Opinion from '../../components/reusableUi/Opinion'
 import { Link } from 'react-router-dom'
+import { useAllOpinionsWithCategories } from '../../hooks/useOpinions'
+import Loading from '../../components/reusableUi/Loading'
 
 export default function Opinions() {
+    const {data , isLoading} = useAllOpinionsWithCategories();
     return (
         <Layout>
             <Container sx={{marginY:"40px"}}>
-                <Box sx={{marginBottom:"40px"}}>
-                    <HeaderTitle title="أحدث الآراء"/>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12} sm={6} md={4} lg={3}><Opinion/></Grid>
-                        <Grid item xs={12} sm={6} md={4} lg={3}><Opinion/></Grid>
-                        <Grid item xs={12} sm={6} md={4} lg={3}><Opinion/></Grid>
-                        <Grid item xs={12} sm={6} md={4} lg={3}><Opinion/></Grid>
-                        <Grid item xs={12} sm={6} md={4} lg={3}><Opinion/></Grid>
-                        <Grid item xs={12} sm={6} md={4} lg={3}><Opinion/></Grid>
-                        <Grid item xs={12} sm={6} md={4} lg={3}><Opinion/></Grid>
-                        <Grid item xs={12} sm={6} md={4} lg={3}><Opinion/></Grid>
-                    </Grid>
-                </Box>
-                <Box sx={{marginBottom:"40px"}}>
-                    <Link to="/opinions/politics"><HeaderTitle title="آراء سياسية"/></Link>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12} sm={6} md={4} lg={3}><Opinion/></Grid>
-                        <Grid item xs={12} sm={6} md={4} lg={3}><Opinion/></Grid>
-                        <Grid item xs={12} sm={6} md={4} lg={3}><Opinion/></Grid>
-                        <Grid item xs={12} sm={6} md={4} lg={3}><Opinion/></Grid>
-                        <Grid item xs={12} sm={6} md={4} lg={3}><Opinion/></Grid>
-                        <Grid item xs={12} sm={6} md={4} lg={3}><Opinion/></Grid>
-                        <Grid item xs={12} sm={6} md={4} lg={3}><Opinion/></Grid>
-                        <Grid item xs={12} sm={6} md={4} lg={3}><Opinion/></Grid>
-                    </Grid>
-                </Box>
+                {
+                    isLoading
+                    ?
+                    <Loading/>
+                    :
+                    data?.categories.map(ca=>{
+                        return <Box sx={{marginBottom:"40px"}} key={ca.id+"mkbh"}>
+                        <Link to={`/opinions/${ca.id}`}>
+                            <HeaderTitle title={ca.title}/>
+                        </Link>
+                        <Grid container spacing={2}>
+                            {
+                                ca.opinions.map(op=>{
+                                    return <Grid key={op.id+"knjh"} item xs={12} sm={6} md={4} lg={3}><Opinion opinion={op}/></Grid>
+                                })
+                            }
+                        </Grid>
+                    </Box>
+                    })
+                }
             </Container>
         </Layout>
     )
